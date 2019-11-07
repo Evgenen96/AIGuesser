630de3c39c2d51a8a180ac507d1bf016cf3ea28e
Eugene Nikonov <NikonovSE96@gmail.com>
2019-11-07 10:24:55 +0400

    initial commit

---
+++ src/Play/Play.java
@@ -0,0 +1,61 @@
+package Play;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Play {
+
+    public static void main(String[] args) throws Exception {
+        TNode top = new TNode();
+        TNode currentNode = top;
+        Scanner sc = new Scanner(System.in, "Cp1251");
+        TNode.reader = new FileReader("tree2.txt");
+        currentNode = top.load();
+        TNode.reader.close();
+        int answer;
+        top = currentNode;
+        System.out.println(top.question);
+        currentNode = currentNode.getAnswer(true);
+        while (true) {
+            if (currentNode.isLastQuestion()) {
+                System.out.println("посл вопрос");
+                System.out.println(currentNode.question);
+                answer = sc.nextInt();
+                if (answer == 1) {
+                    System.out.println("Ура, я угадал");
+                } else {
+                    System.out.println("Кто это тогда?");
+                    String animal = sc.nextLine();
+                    animal = sc.nextLine();
+                    System.out.println("Напишите что отличает его от " + currentNode.question);
+                    String animalDescription = sc.nextLine();
+                    currentNode.parentNode.addNode(animalDescription);
+                    currentNode.parentNode.nodes.get(currentNode.parentNode.nodes.size() - 1).addNode(animal);
+                    TNode.writer  = new FileWriter("tree2.txt", false);
+                    top.save(top);
+                    TNode.writer.close();
+                }
+                break;
+            }
+            if (currentNode.nodesQuestionAllAsked()) {
+                System.out.println("вопросы кончились");
+                System.out.println("Кто это?");
+                String animal = sc.nextLine();
+                animal = sc.nextLine();
+                System.out.println("Как вы бы описали его");
+                String animalDescription = sc.nextLine();
+                currentNode.addNode(animalDescription);
+                currentNode.nodes.get(currentNode.parentNode.nodes.size() - 1).addNode(animal);
+                TNode.writer  = new FileWriter("tree2.txt", false);
+                top.save(top);
+                TNode.writer.close();
+                break;
+            }
+            System.out.println(currentNode.question);
+            answer = sc.nextInt();
+            currentNode.asked = true;
+            currentNode = currentNode.getAnswer(answer == 1);
+        }
+    }
+}
---
+++ src/Play/TNode.java
@@ -0,0 +1,128 @@
+package Play;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TNode {
+
+    public TNode parentNode;
+    public static int counter = 0;
+    public ArrayList<TNode> nodes;
+    public String question;
+    public boolean asked = false;
+    public static FileWriter writer;
+    public static FileReader reader;
+    public static Random random = new Random();
+
+    public TNode() {
+        parentNode = null;
+        question = "Загадайте животное";
+        nodes = new ArrayList();
+    }
+
+    public TNode(TNode parent) {
+        parentNode = parent;
+        nodes = new ArrayList();
+    }
+
+    public TNode(TNode parent, String q) {
+        parentNode = parent;
+        nodes = new ArrayList();
+        question = q;
+    }
+
+//    public TNode(FileWriter fw, FileReader fr) {
+//        parentNode = null;
+//        nodes = new ArrayList();
+//        question = "Загадайте животное";
+//        writer = fw;
+//        reader = fr;
+//    }
+
+    public void addNode() {
+        nodes.add(new TNode(this));
+    }
+
+    public void addNode(String q) {
+        nodes.add(new TNode(this, q));
+    }
+
+    public void save(TNode top) throws IOException {
+        if (top.parentNode != null) {
+            writer.write(top.question + "\n");
+        }
+        ArrayDeque<TNode> queue = new ArrayDeque<>();
+        queue.addAll(top.nodes);
+        while (!queue.isEmpty()) {
+            save(queue.pop());
+        }
+        writer.append("\n");
+    }
+
+    public TNode load() throws IOException {
+        BufferedReader bReader = new BufferedReader(reader);
+        String line;
+        TNode currentNode = new TNode();
+        TNode topNode = currentNode;
+        while ((line = bReader.readLine()) != null) {
+            if (!line.equals("")) {
+                currentNode.addNode(line);
+                currentNode = currentNode.nodes.get(currentNode.nodes.size() - 1);
+            } else if (currentNode.parentNode != null) {
+                currentNode = currentNode.parentNode;
+            }
+        }
+        return topNode;
+    }
+
+    public boolean isLastQuestion() {
+        if (nodes.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean nodesQuestionAllAsked() {
+        if (parentNode == null) {
+            return false;
+        }
+        for (TNode node : nodes) {
+            if (!node.asked) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //случайный выбор вопроса
+    public TNode pickQuestion() {
+        if (nodesQuestionAllAsked()) {
+            return this;
+        }
+        int rndQuestion = -1;
+        while (true) {
+            rndQuestion = random.nextInt(nodes.size());
+            if (!nodes.get(rndQuestion).asked) {
+                break;
+            }
+
+        }
+       
+        return nodes.get(rndQuestion);
+    }
+
+    //получил ответ от пользователя
+    public TNode getAnswer(boolean answer) {
+        if (!answer) {
+            return this.parentNode.pickQuestion();
+        } else {
+            return pickQuestion();
+        }
+    }
+}
